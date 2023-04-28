d7b847ad78c44bebb18e164c39b40fa9471a2305
Lee Eros Galaad Dias De Moura <95001466+LeeEros@users.noreply.github.com>
2023-04-28 15:12:36 -0300

    Fix: jogos

--- forca.py
+++ forca.py
@@ -1,6 +1,6 @@
 def jogar():
     print("*********************************")
-    print("Bem vindo ao jogo da Forca!")
+    print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
     palavra_secreta = "banana"
@@ -8,20 +8,20 @@ def jogar():
     enforcou = False
     acertou = False
 
-    while (not enforcou and not acertou):
+    while(not enforcou and not acertou):
 
-        chute = str(input("Qual a letra? \n"))
+        chute = input("Qual letra? ")
+        chute = chute.strip()
 
         index = 0
         for letra in palavra_secreta:
-            if (chute == letra):
-                print("Escontrei a letra {} na posição {} ".format(letra, index))
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
             index = index + 1
 
-        print("jogando...")
+        print("jogando ...")
 
     print("Fim do jogo")
 
-
-if (__name__ == "__main__"):
+if(__name__ == "__main__"):
     jogar()
--- jogos.py
+++ jogos.py
@@ -1,24 +1,21 @@
 import forca
 import adivinhacao
 
-
 def escolhe_jogo():
     print("*********************************")
-    print("********Esolha o seu jogo!*******")
+    print("*******Escolha o seu jogo!*******")
     print("*********************************")
 
-    print("Jogo 1: Forca\n")
-    print("Jogo 2: Adivinhacao\n")
+    print("(1) Forca (2) Adivinhação")
 
-    jogo = int(input("Qual jogo?\n"))
+    jogo = int(input("Qual jogo? "))
 
-    if (jogo == 1):
-        print("Jogo da forca!")
+    if(jogo == 1):
+        print("Jogando forca")
         forca.jogar()
-    elif (jogo == 2):
-        print("Jogo da adivinhação")
-        import adivinhacao
+    elif(jogo == 2):
+        print("Jogando adivinhação")
         adivinhacao.jogar()
 
-    if (__name__ == "__main__"):
-        escolhe_jogo()
+if(__name__ == "__main__"):
+    escolhe_jogo()
