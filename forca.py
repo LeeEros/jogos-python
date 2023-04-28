5dbe72cf5cb0bf6df69645cff7454eb25cf9863c
Lee Eros Galaad Dias De Moura <95001466+LeeEros@users.noreply.github.com>
2023-04-28 15:32:39 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -8,14 +8,14 @@ def jogar():
     enforcou = False
     acertou = False
 
-    while(not enforcou and not acertou):
+    while (not enforcou and not acertou):
 
         chute = input("Qual letra? ")
         chute = chute.strip()
 
         index = 0
         for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
+            if (chute.upper() == letra.upper()):
                 print("Encontrei a letra {} na posição {}".format(letra, index))
             index = index + 1
 
@@ -23,5 +23,6 @@ def jogar():
 
     print("Fim do jogo")
 
-if(__name__ == "__main__"):
+
+if (__name__ == "__main__"):
     jogar()
